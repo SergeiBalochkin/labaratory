8956161338a198c8fc19fbfc4c7b69dcb248aa4e
SergeiBalochkin <94735301+SergeiBalochkin@users.noreply.github.com>
2022-01-22 06:12:29 +0300

    Update crud.js

--- crud.js
+++ crud.js
@@ -1,7 +1,7 @@
 const rep = require("./repository")
 
-module.exports = new function () {
-    this.get = id => rep.get(id)
+module.exports = new function () {   //module.exports возвращает нам функцию require
+    this.get = id => rep.get(id)    //вызываем функции, написанные в repository
     this.getAll = () => rep.getAll()
     this.create = data => rep.create(data)
     this.update = data => rep.update(data)
